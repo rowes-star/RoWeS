8a2c1274b0d9c944002905511931cbe49de212e2
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-02 14:29:25 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
-OWNER = ["Zo_Mbi_e"]
-OWNER_NAME = "zombie"
-BOT_TOKEN = "6022370529:AAHM5anvuHDDhKN52ypmcoKTLWnWFEPVDDE"
+OWNER = ["R7_OX"]
+OWNER_NAME = "RoWeS"
+BOT_TOKEN = "6484265015:AAHG02-xH7G5Ccrlt_hzy7ajjnHF9CIJ4Do"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
-CHANNEL = "https://t.me/zombieyy"
-GROUP = "https://t.me/zombie2"
-VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
-LOGS = "zombie2"
+CHANNEL = "https://t.me/vc_xm"
+GROUP = "https://t.me/R3_QX"
+VIDEO = "https://telegra.ph/file/ae7a6e68800785b05cde9.jpg"
+LOGS = "e_9cn"
 
