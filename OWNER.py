3cf01ecfd23c29e828554e74ee160c58b60e16c4
RoWeS <129057401+rowes-star@users.noreply.github.com>
2024-06-08 10:50:46 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["R7_OX"]
 OWNER_NAME = "RoWeS"
-BOT_TOKEN = "6484265015:AAHG02-xH7G5Ccrlt_hzy7ajjnHF9CIJ4Do"
+BOT_TOKEN = ":AAHG02-"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/vc_xm"
 GROUP = "https://t.me/R3_QX"
